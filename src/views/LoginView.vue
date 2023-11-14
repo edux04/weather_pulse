33264c4b854fecb7802a4ca32c1236654fad1e09
Eduardo <eduardoramirez0104@gmail.com>
2023-11-14 13:51:10 -0400

    ajuste de las pantallas de auth a 100vh

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -40,10 +40,8 @@ const setError = (errorMessage) => {
 </script>
 
 <template>
-  <section class="bg-gray-50 dark:bg-gray-900">
-    <div
-      class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
-    >
+  <section class="bg-gray-50 dark:bg-gray-900 h-screen">
+    <div class="flex flex-col items-center justify-center mx-auto pt-6">
       <RouterLink
         class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         to="/"
