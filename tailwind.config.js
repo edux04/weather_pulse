56bf4721bd8ddb2a374f7804d798f55e34d74950
Eduardo <eduardoramirez0104@gmail.com>
2023-11-14 13:21:25 -0400

    fix tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
-  ], theme: {
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
